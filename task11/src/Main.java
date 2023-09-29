fca0d44bd8a02c9739a0e2d9c51f9f3355fe2df7
vladimirkuzik <vladimirkuzik574@gmail.com>
2023-09-29 11:23:13 +0300

    test1

--- task11/src/Main.java
+++ task11/src/Main.java
@@ -1,55 +1,44 @@
+// Задание 3.11
 import java.util.Scanner;
 public class Main {
-        public static void main(String[] args)
-        {
-                public class AccountTest {
-                 public static void main(String[] args)
-                {
-                        Main account1 = new Main ("Jane Green", 50.00);
+    private String name;
+    private double balance;
+    public Main(String name, double balance){
+        this.name = name;
+        if (balance > 0.0) {
+            this.balance = balance;
+        }
+    }
+    public void deposit (double depositAmount){
+        if (depositAmount > 0.0) {
+            balance = balance + depositAmount;
+        }
+    }
+    public void withdraw (double withdrawAmount){
+        if (withdrawAmount < balance) {
+            balance = balance - withdrawAmount;
+        } else {
+            System.out.println("Withdrawal amount exceeded account balance");
+        }
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
 
-                         Main account2 = new Main("John Blue", -7.53);
-
-                         // display initial balance of each object
-                         System.out.printf("%s balance: $%.2f%n",
-                                 account1.getName() );  System.out.printf("%s balance: $%.2f%n%n",
-
-                                // display balances
-                                account1.getBalance(),
-                                account2.getName(),
-
-                                account2.getBalance()
-                        );
-                        // create a Scanner to obtain input from the command window
-                        Scanner input = new Scanner(System.in);
-
-                         System.out.print("Enter deposit amount for account1: "); // prompt
-                        double depositAmount = input.nextDouble(); // obtain user input
-
-                        System.out.printf("%nadding %.2f to account1 balance%n%n",
-                                 depositAmount);
-
-                        account1.deposit(depositAmount); // add to account1’s balance
-                        System.out.printf("%s balance: $%.2f%n",
-                                 account1.getName(),
-                                 System.out.printf("%s balance: $%.2f%n%n",
-                                        account2.getName() );
-                        account1.getBalance());
-                        account2.getBalance()
-
-                         System.out.print("Enter deposit amount for account2: "); // prompt
-                        depositAmount = input.nextDouble(); // obtain user input
-
-                        System.out.printf("%nadding %.2f to account2 balance%n%n",
-                                depositAmount);
-
-                        account2.deposit(depositAmount); // add to account2 balance
-
-                         // display balances
-                        System.out.printf("%s balance: $%.2f%n",
-                                 account1.getName(),
-                                account1.getBalance()
-                        );
-                         System.out.printf("%s balance: $%.2f%n%n",
-                                account2.getName(), );
-                } // end main
-                 } // end class AccountTest
+    public static void main(String[] args) {
+        Main Account = new Main("vladimir Cuzic", 2345);
+        int n;
+        Scanner input = new Scanner(System.in);
+        System.out.print("\nСкоко хочешь снять?: ");
+        n = input.nextInt();
+        input.close();
+        Account.withdraw(n);
+        System.out.println(Account.getBalance());
+    }
+}
